3e4e9972dc95380ef1ef25e7c99576048f493381
ashish-am <82749212+ashish-am@users.noreply.github.com>
2022-11-29 19:46:15 +0530

    Add files via upload
    Peer-to-Peer Lending

---
+++ P2P Lending/footer.php
@@ -0,0 +1,15 @@
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright">
+                    &copy; Monsoon Semester Project, DBMS
+                </div>
+                <div class="credits">
+
+                    Developed by <a href="#">Ashish</a> and <a href="#">Samarth</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
